5424b841f7ee4f136ce33b1223c6776039f47248
문영식 <moonyoungsik95@gmail.com>
2025-05-07 18:19:39 +0900

    RoleHirearchImpl 사용

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -2,9 +2,17 @@ package com.example.testsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,13 +25,21 @@ public class SecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+  public RoleHierarchyImpl roleHierarchy() {
+    return RoleHierarchyImpl.fromHierarchy("""
+        ROLE_A > ROLE_B
+        ROLE_B > ROLE_C
+        """);
+  }
 
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
     http
+
         .authorizeHttpRequests((auth) -> auth
-            .requestMatchers("/", "/login", "/loginProc", "/join", "/joinProc").permitAll()
-            .requestMatchers("/admin").hasRole("ADMIN")
-            .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+            .requestMatchers("/", "/login", "/join", "/joinProc", "/logout").permitAll()
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .requestMatchers("/my/**").hasAnyRole("USER")
             .anyRequest().authenticated()
         );
 
@@ -31,21 +47,29 @@ public class SecurityConfig {
         .formLogin((auth) -> auth
             .loginPage("/login")
             .loginProcessingUrl("/loginProc")
+            .defaultSuccessUrl("/")
             .permitAll()
         );
 
 //    http
-//        .csrf((auth) -> auth.disable());
+//        .httpBasic(Customizer.withDefaults());
+
+    http
+        .csrf((auth) -> auth.disable());
+
+    http
+        .logout((auth) -> auth
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/"));
 
     http
         .sessionManagement((auth) -> auth
+            .sessionFixation().changeSessionId()
             .maximumSessions(1)
-            .maxSessionsPreventsLogin(true));
-
-    http
-        .sessionManagement((auth) -> auth
-            .sessionFixation().changeSessionId());
+            .maxSessionsPreventsLogin(true)
+        );
 
     return http.build();
   }
+
 }
--- src/main/java/com/example/testsecurity/controller/MainController.java
+++ src/main/java/com/example/testsecurity/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.example.testsecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,5 +32,16 @@ public class MainController {
         model.addAttribute("role",role);
         return "main";
     }
+
+  @GetMapping("/logout")
+  public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if(authentication != null) {
+      new SecurityContextLogoutHandler().logout(request, response, authentication);
+    }
+
+    return "redirect:/";
+  }
 
 }
